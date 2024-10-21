1a4bf42df339db3ebf17f488fd26ebf86df07ba2
shaoher <shaoher@mail.weber.edu>
2024-10-20 18:48:11 -0600

    added boolean statement, changed level to upper case L so my assignLevel is easier to read

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 // With the following I want to create a project of a real life scenario of a car rental
 // where renters would be directed to what level the rented car is located based on the make, model, and year of the car 
 
-enum level { LEVEL1 = 1, LEVEL2, LEVEL3};
+enum Level { LEVEL1 = 1, LEVEL2, LEVEL3};
 
 // now we are going to create a class for our rental cars along with the private members
 class Car {
@@ -19,8 +19,9 @@ class Car {
     string make;
     string model;
     int year;
-    level level;
+    Level level;
 
 // now we are going to add public members. A public member is accessible from outside the class. 
     public:
         Car(string make, string model, int year) : make(make), model(model), year(year), level {}
+        bool assignLevel() {
