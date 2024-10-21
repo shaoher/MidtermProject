66d0fe3bc186c405086da733c9bfb5ee18350f3e
shaoher <shaoher@mail.weber.edu>
2024-10-20 18:22:32 -0600

    created enumerator named level for the parking level

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+// For this project I need to create a project that has the following: 
+// Class
+// variable
+// Struct or enum
+// function
+// control structure
+// With the following I want to create a project of a real life scenario of a car rental
+// where renters would be directed to what level the rented car is located based on the make, model, and year of the car 
 
-int main(){
-    std::cout << "Hello world" << std::endl;
-    return 0;
-}
+enum level { LEVEL1 = 1, LEVEL2, LEVEL3};
